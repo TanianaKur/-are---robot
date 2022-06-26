19c249827bdcac56dd0fe4eb347cb552dae3b782
TanianaKur <zxzxzx1974@mail.ru>
2022-06-26 20:07:04 +0900

    js цикл

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const userName = 'Волк';
+const sleep = (ms) => {
+	return new Promise((res) => setTimeout(res, ms));
+};
+
+const myAsyncFunc = async () => {
+	for (let num = 0; num < 10; num++) {
+		console.log('Заяц');
+		await sleep(2000);
+		console.log(userName);
+		await sleep(2000);
+	}
+	console.log('Я сломался.....');
+};
+
+myAsyncFunc();
